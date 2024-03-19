feb53c786d8a078e193d26b3677f8f5133053016
bassonk3767 <145512278+bassonk3767@users.noreply.github.com>
2024-03-20 12:31:21 +1300

    Working yes/no & number checker

---
+++ c_03_yes_no_checker_v01.py
@@ -0,0 +1,20 @@
+# Imports...
+
+
+# Functions...
+
+# Checks the user's response
+def yes_no(question):
+
+    to_check = ["yes", "no"]
+
+    valid = false
+    while not valid:
+
+        response = input(question).lower()
+
+        for var_item in to_check:
+
+
+# Main routine...
+
